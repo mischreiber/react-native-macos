2428d74c55f08fd42b091c8a7774692436adfb18
Nick Lefever <lefever@meta.com>
2023-09-13 20:27:11 +0200

    [fabric] Add support for clipsToBounds to RCTViewComponentView
    Summary: This diff conditionally sets the CALayer masksToBounds based on the clipsToBounds RN property so that the content of the view would be clipped by the view border.
    
    Test Plan:
    - Run Zeratul with Fabric enabled
    - Check that the profile pictures with no status indicator are clipped by the half size border radius set on the parent view.
    
    | Before | After |
    |--|
    |  {F1090251649}  |  {F1090249259}  |
    
    Reviewers: shawndempsey, chpurrer, #rn-desktop
    
    Reviewed By: chpurrer
    
    Differential Revision: https://phabricator.intern.facebook.com/D49239973

--- packages/react-native/React/Fabric/Mounting/ComponentViews/View/RCTViewComponentView.mm
+++ packages/react-native/React/Fabric/Mounting/ComponentViews/View/RCTViewComponentView.mm
@@ -862,6 +862,11 @@ static RCTCursor RCTCursorFromCursor(Cursor cursor)
     return;
   }
 
+#if TARGET_OS_OSX // [macOS
+  // clipsToBounds is stubbed out on macOS because it's not part of NSView
+  layer.masksToBounds = self.clipsToBounds;
+#endif // macOS]
+
   const auto borderMetrics = _props->resolveBorderMetrics(_layoutMetrics);
 
   // Stage 1. Shadow Path
