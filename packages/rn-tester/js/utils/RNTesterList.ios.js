3ba19365147861f34fb5690e8cb13e845c07cf44
Saad Najmi <sanajmi@microsoft.com>
2025-03-04 02:33:09 -0600

    chore: remove RCTAddressSanitizerCrashManager (#2395)
    ## Summary:
    
    This module is flagged internally by our static analysis. This module
    and test was added to test that ASAN was actually enabled and would
    crash on unsafe code. This meant checking in unsafe code as a test,
    which is now flagged now that we build from source internally. We don't
    need this module and test anymore, and we never had any automated tests
    that actually checked that this crashes. Let's just delete it.
    
    ## Test Plan:
    
    CI should pass.

--- packages/rn-tester/js/utils/RNTesterList.ios.js
+++ packages/rn-tester/js/utils/RNTesterList.ios.js
@@ -372,10 +372,6 @@ const APIs: Array<RNTesterModuleInfo> = ([
   },
   // [macOS
   {
-    key: 'ASANCrashExample',
-    module: require('../examples/ASAN/ASANCrashExample'),
-  },
-  {
     key: 'Focus Ring',
     module: require('../examples/FocusRing/FocusRingExample'),
   },
